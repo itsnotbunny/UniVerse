0db123fed32c5bca13169aceeee3bc232c7b0680
samuel <samuel.g.priyanshu@gmail.com>
2025-07-21 12:21:35 +0530

    vecel.json changed

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,13 +4,18 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
 import ErrorBoundary from './components/ErrorBoundary'
+import { GoogleOAuthProvider } from '@react-oauth/google'
+
+const clientId = import.meta.env.VITE_GOOGLE_CLIENT_ID; // make sure this is defined in your .env
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ErrorBoundary>
-      <BrowserRouter> 
-        <App />
-      </BrowserRouter>
+      <GoogleOAuthProvider clientId={clientId}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </GoogleOAuthProvider>
     </ErrorBoundary>
   </React.StrictMode>
 )
